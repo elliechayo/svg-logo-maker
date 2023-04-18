e26fa6729803cdf2cad05bedcf71364ef89ba291
Elinor Chayo <elliechayo@gmail.com>
2023-04-17 22:23:19 -0400

    added comments to the code writtens in the folowing files to add clarity to the functionality of the code

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const createSVG = require("./lib/createSVG");
 const writeToFile = require("./lib/writeToFile");
 
 function main() {
+   // prompt the user
   userInput()
     .then((answers) => {
+       // create the svg data
       const svgData = createSVG(answers);
+      // write the svg data to logo.svg file
       writeToFile("logo.svg", svgData);
     })
     .catch((error) => console.error(error));
